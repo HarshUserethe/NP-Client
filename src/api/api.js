0538e92bff9f380731c016655bff79de3792efe1
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2025-01-22 20:55:27 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://paperpulseserver.netlify.app/" });
+const API = axios.create({ baseURL: "https://np-server-tkfl.onrender.com" });
 
 export const getRecords = async () => (await API.get("/")).data;
 export const createRecord = async (data) => (await API.post("/", data)).data;
